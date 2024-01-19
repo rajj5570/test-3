9358a445086ffc66d7f9c00db89ac55b2a42429a
rajib5570 <rajib5570@gmail.com>
2024-01-20 01:29:49 +0600

    File Added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          customButton: '##E76F51',
+          customGreen: '#2ecc71',
+          // Add more custom colors as needed
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
